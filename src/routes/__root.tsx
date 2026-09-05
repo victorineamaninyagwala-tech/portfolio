8a4aa3817accb4fb445d9937325dda66833fe5b9
Victorine Amani Nyagwala <victorinenyagwala@gmail.com>
2026-09-05 13:27:11 +0300

    September

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -93,7 +93,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
         rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Antic&family=Caacupe+One&display=swap",
+        href: "https://fonts.googleapis.com/css2?family=DotGothic16&family=Ubuntu:wght@400;500;700&family=Caacupe+One&display=swap",
       },
       { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
     ],
